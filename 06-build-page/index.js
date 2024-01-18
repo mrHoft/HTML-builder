eca80933752716c770fa3cf74389b5e918564c75
mrHoft <ru.hoft@gmail.com>
2024-01-18 17:43:19 +0300

    feat(06): writeFile, copyFiles, build

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const { readdir } = require('node:fs/promises');
 const path = require('path');
 
+const HASH = '> ';
+
 async function getFilesList(url, ext) {
   try {
     const files = await readdir(url, {
@@ -71,7 +73,9 @@ class mergeStyles {
     const mergedStyles = await this.readFiles(filesList);
     await this.writeMergedStyles(mergedStyles);
 
-    console.log(`Styles was bundled to ./${this.target}/${this.fileName}`);
+    console.log(
+      `${HASH}Styles was bundled to ./${this.target}/${this.fileName}`,
+    );
   }
 }
 
@@ -79,16 +83,19 @@ function rmDir(target) {
   return new Promise((resolve, reject) => {
     fs.rm(path.join(__dirname, target), { recursive: true }, (error) => {
       if (error) return reject(error);
-      console.log('Old project was deleted.');
+      console.log(`${HASH}Deleted: old project files.`);
       resolve();
     });
   });
 }
 
-async function mkDir(target) {
-  fs.mkdir(path.join(__dirname, target), { recursive: true }, (error) => {
-    if (error) return error;
-    console.log(`Directory ${target} created successfully.`);
+async function mkDir(url) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(path.join(url), { recursive: true }, (error) => {
+      if (error) return reject(error);
+      console.log(`${HASH}Created directory: ${url}`);
+      resolve();
+    });
   });
 }
 
@@ -115,38 +122,90 @@ async function readComponents(url) {
   for (const file of fileList) {
     const data = await readFile(path.join(url, file));
     const name = path.basename(file, ext);
-    components[name] = data;
+    components[name] =
+      data[data.length - 1] === '\n' ? data.slice(0, data.length - 1) : data;
   }
   return components;
 }
 
+function writeFile(url, data) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(url, { encoding: 'utf8' });
+    file.on('finish', () => {
+      console.log(`${HASH}Created file: ${url}`);
+      resolve();
+    });
+    for (const bunch of data) {
+      file.write(bunch);
+    }
+    file.end();
+  });
+}
+
+async function copyFiles(from, dest) {
+  await mkDir(dest);
+  return new Promise((resolve, reject) => {
+    readdir(from, { withFileTypes: true }).then((files) => {
+      for (const file of files) {
+        const isDir = file.isDirectory();
+        const [a, b] = [path.join(from, file.name), path.join(dest, file.name)];
+        if (isDir) copyFiles(a, b);
+        else
+          fs.copyFile(a, b, fs.constants.COPYFILE_EXCL, (err) => {
+            if (err) console.log(err);
+            else console.log(`${HASH}Copied: ${b}`);
+          });
+      }
+      resolve();
+    });
+  });
+}
+
 async function build() {
-  const origin = 'styles';
+  const stylesOrigin = 'styles';
   const target = 'project-dist';
-  const stylesName = 'styles.css';
+  const stylesName = 'style.css';
   const indexName = 'index.html';
   const templateName = 'template.html';
 
-  console.log('Bundling project...');
+  console.log('==================\nBundling project...');
   performance.mark('begin');
 
   await rmDir(target).catch((error) => {
     // console.log(error.mssage) // ignore no such folder message
   });
+  await mkDir(path.join(__dirname, target));
 
-  // const template = await readFile(path.join(__dirname, templateName));
-  // console.log(template);
-
+  // Reading template
+  let template = await readFile(path.join(__dirname, templateName));
   const components = await readComponents(path.join(__dirname, 'components'));
-  console.log(components);
 
-  // await mkDir(target);
-  // await new mergeStyles({ origin, target, fileName: stylesName }).perform();
+  // Changing template
+  Object.keys(components).forEach((name) => {
+    template = template.replace(RegExp(`{{${name}}}`, 'm'), components[name]);
+  });
+  console.log(`${HASH}Prepared: ${templateName}`);
+
+  // Wrighting template
+  await writeFile(path.join(__dirname, target, indexName), template);
+
+  // Copy assets
+  await copyFiles(
+    path.join(__dirname, 'assets'),
+    path.join(__dirname, target, 'assets'),
+  );
+
+  // Merge and copy styles
+  await new mergeStyles({
+    origin: stylesOrigin,
+    target,
+    fileName: stylesName,
+  }).perform();
 
   performance.mark('end');
-  const measure = performance.measure('MergeProcess', 'begin', 'end');
+  const measure = performance.measure('BuildProcess', 'begin', 'end');
   const duration = measure.duration.toFixed(2);
-  console.log(`Job is done in ${duration}ms`);
+  console.log(`✨ Job was done in ${duration}ms`);
 }
 
 build();
